b3d88114d33383e736dc998e41d78a4aadac03be
jorj <georgesjarrouj15@gmail.com>
2024-02-04 12:44:08 +0200

    Task CRUD

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -17,20 +17,15 @@ class TaskController extends Controller
         return TaskResource::collection(Task::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
+   
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTaskRequest $request)
     {
-        //
+       $task = Task::create($request->validated());
+       
+       return TaskResource::make($task);
     }
 
     /**
@@ -41,27 +36,24 @@ class TaskController extends Controller
         return TaskResource::make($task);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Task $task)
-    {
-        //
-    }
+    
 
     /**
      * Update the specified resource in storage.
      */
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        $task->update($request->validated());
+        
+        return TaskResource::make($task);
     }
-
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\V1\CompleteTaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('v1')->group(function (){
 
     Route::apiResource('/tasks',TaskController::class);
+
+    Route::patch('/tasks/{task}/complete',CompleteTaskController::class);
 });
